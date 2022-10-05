5bdbc5604d668e7e02aa49d9c4f81e7b3f6237ed
Junio Vitorino <jgvitorino@gmail.com>
2022-10-05 11:06:18 -0300

    refactor: Move to services module

---
+++ app/services/prefix_tree/prefix_tree_service.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'tree'
+
+module PrefixTree
+  # Class Prefix Tree Importer
+  class PrefixTreeService
+    CHUNK_MAX_SIZE = 1024
+
+    def call; end
+
+    def initialize(file_path)
+      @file_chunks = StringIO.new
+      @file = file_path
+      @tree = Tree.new
+    end
+
+    def call!
+      File.open(@file).each(nil, CHUNK_MAX_SIZE) do |chunk|
+        @file_chunks << chunk
+      end
+      parse_json
+    rescue Errno::ENOENT => e
+      puts "Error: #{e}"
+    end
+
+    private
+
+    def parse_json
+      data = JSON.parse(@file_chunks.string)
+      data.each do |key, _value|
+        @tree.insert(key)
+      end
+      @tree
+    end
+  end
+end
--- config/initializers/initialize_prefix_tree.rb
+++ config/initializers/initialize_prefix_tree.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require Rails.root.join('app', 'services', 'prefix_tree_importer')
-
+require Rails.root.join('app', 'services', 'prefix_tree_service.rb')
 JSON_FILE = Rails.root.join('storage', 'names.json')
-PREFIX_TREE = PrefixTreeImporter.new(JSON_FILE).import!
+
+PREFIX_TREE = Services::PrefixTree::PrefixTreeService.new(JSON_FILE).call
